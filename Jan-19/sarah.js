1a5365023bec944fca1a54d2e34f50b37b7a0ff5
abhishekghangas <a.ghangas1@gmail.com>
2022-01-19 22:12:02 +0530

    jan -19

---
+++ Jan-19/sarah.js
@@ -0,0 +1,54 @@
+var reviews = [
+  {
+    id: 1,
+    name: "susan smith",
+    job: "web developer",
+    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  },
+  {
+    id: 2,
+    name: "anna johnson",
+    job: "web designer",
+    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+    text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+  },
+  {
+    id: 3,
+    name: "peter jones",
+    job: "intern",
+    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+  },
+  {
+    id: 4,
+    name: "bill anderson",
+    job: "the boss",
+    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+    text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+  },
+];
+
+var imgElement = document.getElementById("person-img");
+var author = document.getElementById("author");
+var job = document.getElementById("job");
+var info = document.getElementById("info");
+var prevBtn = document.querySelector(".prev-btn");
+var nextBtn = document.querySelector(".next-btn");
+var randomBtn = document.querySelector(".random-btn");
+
+//reviews = [ {}, {} , {}]
+
+var currentIndex = 2;
+
+prevBtn.addEventListener("click", prevClick);
+nextBtn.addEventListener("click", nextClick);
+randomBtn.addEventListener("click", randomaize);
+
+updateUserDetails();
+function updateUserDetails() {
+  imgElement.src = reviews[currentIndex].img;
+  author.innerText = reviews[currentIndex].name;
+  job.innerText = reviews[currentIndex].job;
+  info.innerText = reviews[currentIndex].text;
+}
