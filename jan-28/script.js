dd416bf19f6adb68999397694c036bb140545b6f
sudhanshu kumar <sudhanshukumar8816@gail.com>
2022-01-28 22:53:31 +0530

    jan-28

---
+++ jan-28/script.js
@@ -0,0 +1,91 @@
+'use strict'
+// var x=10
+// console.log(x)
+// x=12
+// console.log(x)
+
+
+// var x=15
+// console.log(x)
+
+
+// let x =10
+// console.log(x)
+// x=12
+// console.log(x)
+
+
+// let x=13
+
+
+// const x=10
+
+// const x=11
+
+
+// console.log(x)
+// var x=11
+// console.log(x)
+
+
+
+// console.log(x)
+// const x=12
+
+// console.log(test)
+// let test =()=>{
+//     console.log(10)
+// }
+
+
+// console.log(test())
+// function test(){
+//     console.log(10)
+// }
+
+
+
+// function test(){
+//     var x=10
+//     console.log(this.x)
+// }
+
+// test()
+
+
+// function holder(){
+//     const test =()=>{
+//         console.log(this)
+//     }
+//     test()
+// }
+
+// holder()
+
+
+// var x=43
+// const obj={
+//     printX:function(){
+//         console.log(this.x)
+//     }
+// }
+
+// obj.printX()
+
+// function test(){
+//     console.log(this)
+// }
+// const x=17
+// const test =()=>{
+//     console.log(this)
+// }
+
+// test()
+
+// (
+//     ()=>{
+//         console.log("hi")
+//     }
+// )()
+
+
