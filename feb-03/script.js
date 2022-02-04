9bb453ca868680a23ed4d0061b9c116dfe2d76e3
sudhanshu kumar <sudhanshukumar8816@gail.com>
2022-02-04 09:41:42 +0530

    feb-03

---
+++ feb-03/script.js
@@ -0,0 +1,58 @@
+
+// var arr=[1,2,3,4,5,6,34,45,56,78,90,0]
+
+// const timer=  setTimeout((a,b)=>{
+//     var ans =1;
+//     for (const val of arr){
+//         ans*=val
+//     }
+//     console.log(ans)
+// },3000,2,3)
+
+
+// if(arr.includes(0)){
+//     clearTimeout(timer)
+//     console.log(0)
+// }
+
+
+
+// setInterval((a,b)=>{
+//     console.log(a+b)
+//     console.log("running every 1 sec")
+// },1000,2,3)
+
+// setTimeout(()=>{
+//     clearInterval(interval)
+//     setInterval(()=>{
+//         console.log("hi")
+//     },1000)
+// },4000)
+
+// const interval =setInterval(()=>{
+//     console.log("hi")
+// },1000)
+
+// rest pattern and parameters
+
+
+// function add(a,b,...sid){
+//     console.log(sid)
+//     let ans =0
+//     ans=ans+a+b
+//     for(var i=0;i<sid.length;i++){
+//         ans+=sid[i]
+//     }
+//     console.log(ans)
+// }
+
+
+// add(2,3,15)
+
+
+
+// var a = -1 | 1;
+
+
+// var b= 2 || 3
+
